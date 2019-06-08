cb984593f07c42f97fcf673171d64f3923040ac8
Hank Grabowski <hankgrabowski@gmail.com>
2019-06-07 23:34:31 -0400

    Add DesignDataExample for 3 June 2019 Blog Post

---
+++ DesignDataExample/AvaloniaApplication1/DesignData/TestGreeting.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvaloniaApplication1.DesignData
+{
+    class TestGreeting
+    {
+        public string Greeting => "Hello from the designer!";
+    }
+}
