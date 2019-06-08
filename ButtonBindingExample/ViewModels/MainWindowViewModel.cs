6f87d55ad4bbe6340dd07263b3f24517725fa053
Hank Grabowski <hankgrabowski@gmail.com>
2019-06-08 15:06:51 -0400

    Add Simple Update Method

--- ButtonBindingExample/ViewModels/MainWindowViewModel.cs
+++ ButtonBindingExample/ViewModels/MainWindowViewModel.cs
@@ -14,9 +14,14 @@ namespace ButtonBindingExample.ViewModels
             set => this.RaiseAndSetIfChanged(ref clickStatusUpdate, value);
         }
 
+        public void UpdateFromSimpleMethod()
+        {
+            UpdateTextStatus("Simple Method Button");
+        }
+
         public void UpdateTextStatus(string nameOfUpdater)
         {
-            ClickStatusUpdate = $"{nameOfUpdater} updated me at ${DateTime.Now}";
+            ClickStatusUpdate = $"{nameOfUpdater} updated me at {DateTime.Now}";
         }
     }
 }
