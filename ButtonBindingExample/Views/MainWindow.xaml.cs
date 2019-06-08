69e9a7c977a995d0451fdf8fb3b77998791ca9eb
Hank Grabowski <hankgrabowski@gmail.com>
2019-06-08 15:46:42 -0400

    Add Click Methods

--- ButtonBindingExample/Views/MainWindow.xaml.cs
+++ ButtonBindingExample/Views/MainWindow.xaml.cs
@@ -1,22 +1,34 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using ButtonBindingExample.ViewModels;
 using SharpDX;
 
 namespace ButtonBindingExample.Views
 {
     public class MainWindow : Window
     {
-        
+        private MainWindowViewModel SpecificViewModel => DataContext as MainWindowViewModel;
+
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = "hello";
+            var clickCodeButton = this.Find<Button>("ClickCodeButton");
+            clickCodeButton.Click += delegate
+            {
+                SpecificViewModel.UpdateTextStatus("On Click Code Button");
+            };
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            SpecificViewModel.UpdateTextStatus("On Click XAML Button");
         }
     }
 }
