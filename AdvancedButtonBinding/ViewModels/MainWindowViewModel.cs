feba131ed6fb79f83f70376292e5ef683f7dc496
Hank Grabowski <hankgrabowski@gmail.com>
2019-06-09 11:27:31 -0400

    Add AdvancedButtonBinding

---
+++ AdvancedButtonBinding/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Text;
+using ReactiveUI;
+
+namespace AdvancedButtonBinding.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string greeting;
+        public string Greeting
+        {
+            get => greeting;
+            set => this.RaiseAndSetIfChanged(ref greeting, value);
+        }
+        
+        private string name;
+        public string Name
+        {
+            get => name;
+            set => this.RaiseAndSetIfChanged(ref name, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            var buttonEnabled = this.WhenAnyValue(
+                x => x.Name,
+                x => !string.IsNullOrWhiteSpace(x)); 
+            WriteGreetingReactiveCommand = ReactiveCommand.Create<string>(name => { WriteGreeting(name); }, buttonEnabled);
+        }
+
+        public ReactiveCommand WriteGreetingReactiveCommand { get; }
+
+        public void WriteGreeting(string personsName)
+        {
+            Greeting = $"Hello {personsName}!";   
+        }
+    }
+}
