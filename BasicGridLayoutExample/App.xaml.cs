ae8272fdd14f26aff134f19c04258fa426bde8f1
Hank Grabowski <hankgrabowski@gmail.com>
2019-10-26 10:02:02 -0400

    Update BasicGridLayoutExample to new Avalonia lifetime process.

--- BasicGridLayoutExample/App.xaml.cs
+++ BasicGridLayoutExample/App.xaml.cs
@@ -1,5 +1,8 @@
 using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using BasicGridLayoutExample.ViewModels;
+using BasicGridLayoutExample.Views;
 
 namespace BasicGridLayoutExample
 {
@@ -9,5 +12,19 @@ namespace BasicGridLayoutExample
         {
             AvaloniaXamlLoader.Load(this);
         }
-   }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            var window = new MainWindow
+            {
+                DataContext = new MainWindowViewModel(),
+            };
+
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+                desktop.MainWindow = window;
+            else if (ApplicationLifetime is ISingleViewApplicationLifetime singleView)
+                singleView.MainView = window;
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
 }
