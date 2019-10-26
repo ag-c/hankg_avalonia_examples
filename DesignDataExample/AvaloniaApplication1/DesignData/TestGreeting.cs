e8581e65964b56beecda6fe6240be6e69143d9cd
HankG <hankgrabowski@gmail.com>
2019-10-26 11:34:48 -0400

    Update all to WinExe and update DesignData to have public DD class

--- DesignDataExample/AvaloniaApplication1/DesignData/TestGreeting.cs
+++ DesignDataExample/AvaloniaApplication1/DesignData/TestGreeting.cs
@@ -1,11 +1,14 @@
+using AvaloniaApplication1.ViewModels;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AvaloniaApplication1.DesignData
 {
-    class TestGreeting
+    public class TestGreeting
     {
         public string Greeting => "Hello from the designer!";
+
     }
 }
